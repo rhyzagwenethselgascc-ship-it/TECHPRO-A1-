b97cd4e07e718176f6c1d6ee5f920f287bf36bb8
rhyzagwenethselgascc-ship-it <rhyzagwenethselga.scc@gmail.com>
2025-09-05 08:35:10 +0000

    done

---
+++ SeatworkSeven.java
@@ -0,0 +1,28 @@
+public class SeatworkSeven {
+        public static void main(String[] args) {
+
+                char grade = 'C';
+
+                        switch (grade) {
+                                    case 'A':
+                                                    System.out.println("Excellent");
+                                                                    break;
+                                                                                case 'B':
+                                                                                                System.out.println("Good");
+                                                                                                                break;
+                                                                                                                            case 'C':
+                                                                                                                                            System.out.println("Well Done");
+                                                                                                                                                            break;
+                                                                                                                                                                        case 'D':
+                                                                                                                                                                                        System.out.println("You passed");
+                                                                                                                                                                                                        break;
+                                                                                                                                                                                                                    case 'F':
+                                                                                                                                                                                                                                    System.out.println("Better try again");
+                                                                                                                                                                                                                                                    break;
+                                                                                                                                                                                                                                                                default:
+                                                                                                                                                                                                                                                                                System.out.println("Invalid grade");
+                                                                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                                                                                System.out.println("Your grade is: " + grade);
+                                                                                                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                                                                                                    }
+}
