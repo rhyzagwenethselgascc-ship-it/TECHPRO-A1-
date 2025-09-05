c99c5ca34bb6bd010334bad2fcaf21ca022ab7b7
rhyzagwenethselgascc-ship-it <rhyzagwenethselga.scc@gmail.com>
2025-09-05 10:17:19 +0000

    done

---
+++ SeatworkFive.java
@@ -0,0 +1,17 @@
+public class Main {
+        public static void main(String[] args) {
+
+                    System.out.println("Maria Juliana P Cardiño");
+                            int countdown = 3;
+
+                                    while (countdown > 0 ) {
+                                                    System.out.println(countdown);
+                                                                countdown--;
+                                    }
+
+                                            System.out.println("Happy new year!");
+                                }
+                            }
+                                    }
+        }
+}
