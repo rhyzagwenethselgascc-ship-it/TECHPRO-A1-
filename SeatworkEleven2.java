c6e97be2030fe2c99ed78a5130b70f0cbbc293d3
rhyzagwenethselgascc-ship-it <rhyzagwenethselga.scc@gmail.com>
2025-09-08 02:02:12 +0000

    dome

---
+++ SeatworkEleven2.java
@@ -0,0 +1,13 @@
+public class SeatworkEleven2 {
+            public static void main(String[] args) {
+                System.out.println("SEATWORKELEVENPART2");
+                    int[][] myNumbers = {
+                                {1, 4, 2},
+                                            {3, 6, 8}
+                                                    };
+
+                                                            myNumbers[1][2] = 9;
+
+                                                                    System.out.println(myNumbers[1][2]);
+                                                                        }
+                                                                        }
