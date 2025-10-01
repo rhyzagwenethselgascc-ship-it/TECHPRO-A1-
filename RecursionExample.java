5b7530abb916af7bfa0e2379c22fa956234798b7
rhyzagwenethselgascc-ship-it <rhyzagwenethselga.scc@gmail.com>
2025-10-01 09:36:57 +0000

    done

---
+++ RecursionExample.java
@@ -0,0 +1,14 @@
+public class RecursionExample
+{
+	public static void sayHello(int count) {
+	if (count == 0) {// halting condition
+	    System.out.println("Done!");
+	} else {
+	    System.out.println("Hello");
+	    sayHello(count-1);//recursive call
+	}
+	}
+	 public static void main(String[] args){
+    sayHello(3);// prints Hello 3 times
+}
+}
