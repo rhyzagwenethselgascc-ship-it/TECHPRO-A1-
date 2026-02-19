72bf59d65099eb0db500c4a5f7a0b617aced0c17
rhyzagwenethselgascc-ship-it <rhyzagwenethselga.scc@gmail.com>
2026-02-19 08:54:16 +0000

    done

---
+++ Seatwork10.java
@@ -0,0 +1,13 @@
+public class Program {
+    public static void main(String[] args) {
+        try {
+            int[] x = {1, 2};   
+            System.out.println(x[5]); 
+            
+        } catch (ArithmeticException e) {
+            System.out.println("Math Error");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array error");
+        }
+    }
+}
